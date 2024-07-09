39f20c9ad24651ac9320e94cf7d19a67177a36a6
Shiyuan Zhou <shizhou2@student.unimelb.edu.au>
2024-07-10 02:06:02 +1000

    edited logo

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -8,11 +8,9 @@ export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
       <span className={styles.date}>
-        {`© ${new Date().getFullYear()} ${config.name}.`}
+        {`© ${new Date().getFullYear()} Built by ${config.name} with ❤️`}
       </span>
-      <Link secondary className={styles.link} href="/humans.txt" target="_self">
-        Crafted by yours truly
-      </Link>
+      <p style={{ color: '#3b3b3b' }}>Speacial thank to HamishMW</p>
     </Text>
   </footer>
 );
